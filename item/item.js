67f98dd3e7c5d1b0a6d58b10b8a334a0891cd5a8
ilqar012 <ilqarnuriyev055@gmail.com>
2024-04-15 19:49:01 +0400

    added

---
+++ item/item.js
@@ -0,0 +1,26 @@
+// const inc = document.querySelector(".inc");
+// const dec = document.querySelector(".dec");
+// const result = document.querySelector(".result");
+// const reset = document.querySelector(".reset");
+
+// let count = localStorage.getItem("count") || 0;
+// result.textContent = count;
+
+// inc.addEventListener("click", function () {
+//   count++;
+//   result.textContent = count;
+//   localStorage.setItem("count", count);
+// });
+
+// dec.addEventListener("click", function () {
+//   count--;
+//   result.textContent = count;
+//   localStorage.setItem("count", count);
+// });
+
+// reset.addEventListener("click", function () {
+//   count = 0;
+//   result.textContent = count;
+//   localStorage.setItem("count", count);
+
+// });
